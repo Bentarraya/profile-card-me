136a29c6ef5c34f8e795d5e0a39a701083761f1e
bentarraya <bentarraya01@gmail.com>
2023-03-30 20:50:32 +0700

    change all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 /*=============== SHOW SOCIAL NETWORKS ===============*/
-const showSocial = (toggleCard, socialCard) =>{
-    const toggle = document.getElementById(toggleCard),
-          social = document.getElementById(socialCard)
-          
-    toggle.addEventListener('click', () =>{
-        if(social.classList.contains('animation')){
-            social.classList.add('down-animation')
-            setTimeout(() =>{
-                social.classList.remove('down-animation')
-            }, 1000)
-        }
+const showSocial = (toggleCard, socialCard) => {
+  const toggle = document.getElementById(toggleCard),
+    social = document.getElementById(socialCard);
 
-        social.classList.toggle('animation')
-    })
-}
-showSocial('card-toggle', 'card-social')
+  toggle.addEventListener("click", () => {
+    if (social.classList.contains("animation")) {
+      social.classList.add("down-animation");
+      setTimeout(() => {
+        social.classList.remove("down-animation");
+      }, 1000);
+    }
+
+    social.classList.toggle("animation");
+  });
+};
+showSocial("card-toggle", "card-social");
